0199ffcc6a33ff9b9e3f21606712a1d62ea7b024
Yuliia Kozachko <ykozachko@3ipk.com>
2024-09-26 17:06:06 +0200

    Add validation; display validation errors; permit attributes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,18 +12,29 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create(
+    # @item = Item.create(
 
-    name: params[:item][:name],
-    price: params[:item][:price]
-    )
+    # name: params[:item][:name],
+    # price: params[:item][:price]
+    # )
+    @item = Item.new item_attributes
 
-    redirect_to item_url(@item)
+    if @item.save
+      redirect_to item_url(@item)
+    else
+      render "new", status: :unprocessable_entity
+    end
   end
 
   def destroy
     @item = Item.find params[:id]
     @item.destroy
-    redirect_to items_url, notice: 'Item was successfully deleted.'
+    redirect_to items_url
+  end
+
+  private
+
+  def item_attributes
+    params.require(:item).permit(:name, :price)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,3 @@
 class Item < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 10 }
 end
