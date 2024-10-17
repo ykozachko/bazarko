a6b34cc973ff82d7911ceb12ece72e2304fc5107
Yuliia Kozachko <ykozachko@3ipk.com>
2024-10-17 16:27:14 +0200

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  root 'items#index'
+  root 'pages#index'
 
   resources :items, only: %i[index show new create destroy edit update]
   resources :categories
