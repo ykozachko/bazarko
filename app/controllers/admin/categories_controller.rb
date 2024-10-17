1dd2287369b7f080bc620834abfbb348e5bc7a5c
Yuliia Kozachko <ykozachko@3ipk.com>
2024-10-17 17:07:44 +0200

    New Admin namespace

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,49 @@
+class Admin::CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @category = Category.find params[:id]
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new category_attributes
+
+    if @category.save
+      redirect_to admin_category_url(@category)
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @category = Category.find params[:id]
+  end
+
+  def update
+    @category = Category.find params[:id]
+
+    if @category.update category_attributes
+      redirect_to admin_category_url(@category)
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category = Category.find params[:id]
+    @category.destroy
+    redirect_to categories_url
+  end
+
+  private
+
+  def category_attributes
+    params.require(:category).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,13 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   resources :items, only: %i[index show new create destroy edit update]
-  resources :categories
+  resources :categories, only: %i[index show new create destroy edit update]
 
   get 'top-inzerat/:id', to: 'items#show'
 
   get 'kontact', to: 'pages#contact', as: :contact
+
+  namespace :admin do
+    resources :categories
+  end
 end
