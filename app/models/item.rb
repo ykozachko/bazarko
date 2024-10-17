580f234c183b63dd7c199188cb25e5f8da580c8b
Yuliia Kozachko <ykozachko@3ipk.com>
2024-10-17 15:31:29 +0200

    Bootstrap setup

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :category
 
-  validates :name, presence: true, length: { minimum: 10 }
+  validates :name, presence: true, length: { maximum: 20 }
   validates :price, presence: true
 end
