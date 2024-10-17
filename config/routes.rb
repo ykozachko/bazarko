ab358ce4a6268b9859ac4ab19d2b6bba8e79119e
Yuliia Kozachko <ykozachko@3ipk.com>
2024-10-17 16:25:31 +0200

    Home and Contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'items#index'
 
   resources :items, only: %i[index show new create destroy edit update]
   resources :categories
 
   get 'top-inzerat/:id', to: 'items#show'
-  # get 'filter/:status', to: 'items#show)'
+
+  get 'kontact', to: 'pages#contact', as: :contact
 end
