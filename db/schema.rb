290cef55bb314b629ea42595c790c80f059ab839
Yuliia Kozachko <ykozachko@3ipk.com>
2024-11-14 16:14:01 +0100

    update rails from 7.0 to 7.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_31_152101) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_14_150157) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
