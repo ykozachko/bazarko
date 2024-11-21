bfb3137c488b04843ccf339d0ee17f11e91feaa5
Yuliia Kozachko <ykozachko@3ipk.com>
2024-11-21 15:24:18 +0100

    fix form

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :category
   belongs_to :user
+  has_many_attached :images
 
   validates :name, presence: true, length: { maximum: 20 }
   validates :price, presence: true
-
-  has_many_attached :images
 end
