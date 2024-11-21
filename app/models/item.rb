2bba3c89a2e9d296659afb1fe33214bc52354c83
Yuliia Kozachko <ykozachko@3ipk.com>
2024-11-21 15:58:57 +0100

    fix images styles

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many_attached :images do |attachable|
     attachable.variant :thumb, resize_to_limit: [200, 200]
+    attachable.variant :normal, resize_to_limit: [800, 800]
   end
 
   validates :name, presence: true, length: { maximum: 20 }
